8e862e55919870c8441e59d47d426e62dd756734
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2023-07-27 00:56:33 -0300

    Add files via upload

---
+++ 1COP005_algoritmos/jogo_vida/headers/vida.h
@@ -0,0 +1,19 @@
+#ifndef VIDA_INCLUDED
+#define VIDA_INCLUDED
+#define TAM 30
+
+#include <stdio.h>
+
+typedef int **Matriz;
+
+int vizinhos(Matriz, int, int);
+
+int numLinhas(FILE *file);
+
+void leitura(int, char *, Matriz *, Matriz *);
+
+void imprimir(Matriz, Matriz, int);
+
+void reiniciar(Matriz, Matriz);
+
+#endif
