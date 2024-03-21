7a31cfc7678df3fa9a8598bf27f8a657f2139756
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2024-03-21 20:31:47 -0300

    Delete 1COP013_LAB/carrinhoSESSION directory

--- 1COP013_LAB/carrinhoSESSION/src/main/java/uel/br/produtos/ServletInitializer.java
+++
@@ -1,9 +0,0 @@
-package uel.br.produtos;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-public class ServletInitializer extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ProdutosApplication.class);
-    }
-}
